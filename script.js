aba49cdd16d0d2198e1126b5cc066357541d968f
cusersvns-dc <cusersvns@gmail.com>
2025-02-08 11:12:50 +0100

    p

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.typing-text span', {
-    strings: ['Student', 'Web Developer', 'Just a chill guy'],
+    strings: ['Student', 'Web Developper', 'Just a chill guy'],
     typeSpeed: 100,
     backSpeed: 50,
     loop: true
