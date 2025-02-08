287492bcdddb55c88ff57b5e486a4fba0e6dc450
cusersvns-dc <cusersvns@gmail.com>
2025-02-08 11:11:44 +0100

    changed something

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.typing-text span', {
-    strings: ['Student', 'Beginner Web Developer', 'Just a chill guy'],
+    strings: ['Student', 'Web Developer', 'Just a chill guy'],
     typeSpeed: 100,
     backSpeed: 50,
     loop: true
