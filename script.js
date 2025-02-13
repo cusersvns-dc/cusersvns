f100df5a009e95db793b1aea720fb165b97e323d
cusersvns-dc <cusersvns@gmail.com>
2025-02-13 12:24:58 +0100

    deleted all

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.typing-text span', {
-    strings: ['Student', 'Web Developper', 'Just a chill guy'],
+    strings: ['Student', 'Just a chill guy'],
     typeSpeed: 100,
     backSpeed: 50,
     loop: true
