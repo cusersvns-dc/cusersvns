457e4acd9f1805154b36ceeff7f8b878abe4dd50
cusersvns-dc <cusersvns@gmail.com>
2026-06-24 17:51:12 +0200

    altro

--- src/lib/content.js
+++ src/lib/content.js
@@ -76,7 +76,7 @@ export const projects = [
     category: 'RPG UI',
     image: 'https://cusersvns.com/images/projects/golden-age/1.png',
     media: [
-      'https://cusersvns.com/images/projects/golden-age/Animation-1.mp4',
+      'https://cusersvns.com/images/projects/golden-age/AltroAnimation-1.mp4',
       'https://cusersvns.com/images/projects/golden-age/1.png',
       'https://cusersvns.com/images/projects/golden-age/2.png',
       'https://cusersvns.com/images/projects/golden-age/3.png',
